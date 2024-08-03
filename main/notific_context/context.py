01747f581d166df2bc0998cb21822a36e8ce7c5d
Krd2024 <161505798+Krd2024@users.noreply.github.com>
2024-08-03 23:07:37 +0300

    Update context.py

--- main/notific_context/context.py
+++ main/notific_context/context.py
@@ -2,5 +2,10 @@ from main.models import Notification
 
 
 def latest_notific(request):
-    latest_notific = Notification.objects.filter(recipient=request.user, is_read=False)
-    return {"latest_notific": latest_notific}
+    try:
+        latest_notific = Notification.objects.filter(
+            recipient=request.user, is_read=False
+        )
+        return {"latest_notific": latest_notific}
+    except:
+        ...
